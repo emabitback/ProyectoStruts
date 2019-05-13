4f492dfbe920de820fc3a1358a7e93579f94b6e6
bitback <emanuel_ramirez123@hotmail.com>
2019-05-12 22:43:12 -0500

    Primer Repositorio de Netbeans a GitHub

---
+++ ProyectoStruts/src/java/devel/action/ProductoAction.java
@@ -0,0 +1,58 @@
+
+package devel.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.actions.DispatchAction;
+
+import dao.ProductosDAO;
+import devel.form.ProductoForm;
+import javax.servlet.ServletException;
+
+/**
+ *
+ * @author bitback
+ */
+public class ProductoAction extends DispatchAction {
+
+    /* forward name="success" path="" */
+    private static final String SUCCESS = "success";
+    
+     public ActionForward doRegistro(ActionMapping mapping, ActionForm form,
+            HttpServletRequest request, HttpServletResponse response)
+            throws ServletException{
+        ProductoForm pf= (ProductoForm) form;
+        
+        try{
+            ProductoDAO pDAO = new ProductoDAO();
+            
+           
+        }catch(Exception e){
+            
+        }
+        //return mapping.findForward(Loguea);
+        return null;
+    }
+
+    /**
+     * This is the action called from the Struts framework.
+     *
+     * @param mapping The ActionMapping used to select this instance.
+     * @param form The optional ActionForm bean for this request.
+     * @param request The HTTP Request we are processing.
+     * @param response The HTTP Response we are processing.
+     * @throws java.lang.Exception
+     * @return
+     */
+   /* @Override
+    public ActionForward execute(ActionMapping mapping, ActionForm form,
+            HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
+        
+        return mapping.findForward(SUCCESS);
+    }
+    */
+}
